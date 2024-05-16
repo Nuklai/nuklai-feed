454dc69d1d4aaf45c058d8384d4a3645094c8f27
Kiran Pachhai <kiran.pachhai@gmail.com>
2024-05-16 11:51:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Nuklai Feed
+
+## Build
+
+```bash
+./scripts/build.sh
+```
+
+## Run
+
+```bash
+./build/nuklai-feed ./config.json
+```
---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Copyright (C) 2024, AllianceBlock. All rights reserved.
+# See the file LICENSE for licensing terms.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Set the CGO flags to use the portable version of BLST
+#
+# We use "export" here instead of just setting a bash variable because we need
+# to pass this flag to all child processes spawned by the shell.
+export CGO_CFLAGS="-O -D__BLST_PORTABLE__" CGO_ENABLED=1
+
+# Root directory
+ROOT_PATH=$(
+    cd "$(dirname "${BASH_SOURCE[0]}")"
+    cd .. && pwd
+)
+
+# Set default binary directory location
+FEED_PATH=$ROOT_PATH/build/nuklai-feed
+
+echo "Building nuklai-feed in $FEED_PATH"
+mkdir -p "$(dirname "$FEED_PATH")"
+go build -o "$FEED_PATH" ./
