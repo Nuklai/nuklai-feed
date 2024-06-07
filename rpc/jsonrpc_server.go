a5e111751458d3361bde5dd0ed06ca4c12211ee1
Kiran Pachhai <kiran.pachhai@gmail.com>
2024-06-07 16:39:41 -0400

    Reverting some changes

--- rpc/jsonrpc_server.go
+++ rpc/jsonrpc_server.go
@@ -1,8 +1,8 @@
 package rpc
 
 import (
-	"context"
-	"encoding/json"
+	"errors"
+	"net/http"
 
 	"github.com/ava-labs/hypersdk/codec"
 	"github.com/nuklai/nuklai-feed/manager"
@@ -10,81 +10,11 @@ import (
 )
 
 type JSONRPCServer struct {
-	m *manager.Manager
+	m Manager
 }
 
-func NewJSONRPCServer(m *manager.Manager) *JSONRPCServer {
+func NewJSONRPCServer(m Manager) *JSONRPCServer {
 	return &JSONRPCServer{m}
-}
-
-type JSONRPCRequest struct {
-	JSONRPC string          `json:"jsonrpc"`
-	Method  string          `json:"method"`
-	Params  json.RawMessage `json:"params"`
-	ID      interface{}     `json:"id"`
-}
-
-type JSONRPCResponse struct {
-	JSONRPC string        `json:"jsonrpc"`
-	Result  interface{}   `json:"result,omitempty"`
-	Error   *jsonrpcError `json:"error,omitempty"`
-	ID      interface{}   `json:"id"`
-}
-
-type jsonrpcError struct {
-	Code    int         `json:"code"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-}
-
-func (j *JSONRPCServer) HandleRequest(req JSONRPCRequest) JSONRPCResponse {
-	var result interface{}
-	var jsonErr *jsonrpcError
-
-	switch req.Method {
-	case "feedInfo":
-		var params struct{}
-		err := json.Unmarshal(req.Params, &params)
-		if err != nil {
-			jsonErr = &jsonrpcError{Code: -32602, Message: "Invalid params"}
-			break
-		}
-		var reply FeedInfoReply
-		jsonErr = j.FeedInfo(params, &reply)
-		result = reply
-
-	case "feed":
-		var params FeedArgs
-		err := json.Unmarshal(req.Params, &params)
-		if err != nil {
-			jsonErr = &jsonrpcError{Code: -32602, Message: "Invalid params"}
-			break
-		}
-		var reply FeedReply
-		jsonErr = j.Feed(params, &reply)
-		result = reply
-
-	case "updateNuklaiRPC":
-		var params UpdateNuklaiRPCArgs
-		err := json.Unmarshal(req.Params, &params)
-		if err != nil {
-			jsonErr = &jsonrpcError{Code: -32602, Message: "Invalid params"}
-			break
-		}
-		var reply UpdateNuklaiRPCReply
-		jsonErr = j.UpdateNuklaiRPC(params, &reply)
-		result = reply
-
-	default:
-		jsonErr = &jsonrpcError{Code: -32601, Message: "Method not found"}
-	}
-
-	return JSONRPCResponse{
-		JSONRPC: "2.0",
-		Result:  result,
-		Error:   jsonErr,
-		ID:      req.ID,
-	}
 }
 
 type FeedInfoReply struct {
@@ -92,10 +22,10 @@ type FeedInfoReply struct {
 	Fee     uint64 `json:"fee"`
 }
 
-func (j *JSONRPCServer) FeedInfo(_ struct{}, reply *FeedInfoReply) *jsonrpcError {
-	addr, fee, err := j.m.GetFeedInfo(context.Background())
+func (j *JSONRPCServer) FeedInfo(req *http.Request, _ *struct{}, reply *FeedInfoReply) (err error) {
+	addr, fee, err := j.m.GetFeedInfo(req.Context())
 	if err != nil {
-		return &jsonrpcError{Code: -32000, Message: err.Error()}
+		return err
 	}
 	reply.Address = codec.MustAddressBech32(consts.HRP, addr)
 	reply.Fee = fee
@@ -112,10 +42,10 @@ type FeedReply struct {
 	Feed []*manager.FeedObject `json:"feed"`
 }
 
-func (j *JSONRPCServer) Feed(args FeedArgs, reply *FeedReply) *jsonrpcError {
-	feed, err := j.m.GetFeed(context.Background(), args.SubnetID, args.ChainID, args.Limit)
+func (j *JSONRPCServer) Feed(req *http.Request, args *FeedArgs, reply *FeedReply) (err error) {
+	feed, err := j.m.GetFeed(req.Context(), args.SubnetID, args.ChainID, args.Limit)
 	if err != nil {
-		return &jsonrpcError{Code: -32000, Message: err.Error()}
+		return err
 	}
 	reply.Feed = feed
 	return nil
@@ -130,13 +60,13 @@ type UpdateNuklaiRPCReply struct {
 	Success bool `json:"success"`
 }
 
-func (j *JSONRPCServer) UpdateNuklaiRPC(args UpdateNuklaiRPCArgs, reply *UpdateNuklaiRPCReply) *jsonrpcError {
+func (j *JSONRPCServer) UpdateNuklaiRPC(req *http.Request, args *UpdateNuklaiRPCArgs, reply *UpdateNuklaiRPCReply) error {
 	if args.AdminToken != j.m.Config().AdminToken {
-		return &jsonrpcError{Code: -32000, Message: "unauthorized user"}
+		return errors.New("unauthorized user")
 	}
-	err := j.m.UpdateNuklaiRPC(context.Background(), args.NuklaiRPCUrl)
+	err := j.m.UpdateNuklaiRPC(req.Context(), args.NuklaiRPCUrl)
 	if err != nil {
-		return &jsonrpcError{Code: -32000, Message: err.Error()}
+		return err
 	}
 	reply.Success = true
 	return nil
